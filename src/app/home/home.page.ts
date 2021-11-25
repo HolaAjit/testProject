c43c9c411a4e9d1fb988e8024da20cd742352624
ajit <ajit.chaudhary@holasure.com>
2021-11-25 17:03:33 +0530

    scan code

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import { Geolocation, Position } from '@capacitor/geolocation';
 import { Share } from '@capacitor/share';
+import { ToastController, LoadingController, Platform } from '@ionic/angular';
+import jsQR from 'jsqr';
 
 @Component({
   selector: 'app-home',
@@ -10,10 +12,38 @@ import { Share } from '@capacitor/share';
 })
 export class HomePage {
 
+  @ViewChild('video', { static: false }) video: ElementRef;
+  @ViewChild('canvas', { static: false }) canvas: ElementRef;
+  @ViewChild('fileinput', { static: false }) fileinput: ElementRef;
+
+  canvasElement: any;
+  videoElement: any;
+  canvasContext: any;
+  scanActive = false;
+  scanResult = null;
+  loading: HTMLIonLoadingElement = null;
+
   myImage: string = null;
   position: Position = null;
 
-  constructor() {}
+  constructor(
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
+    private plt: Platform
+  ) {
+    const isInStandaloneMode = () =>
+      'standalone' in window.navigator && window.navigator['standalone'];
+    if (this.plt.is('ios') && isInStandaloneMode()) {
+      console.log('I am a an iOS PWA!');
+      // E.g. hide the scan functionality!
+    }
+  }
+
+  ngAfterViewInit() {
+    this.canvasElement = this.canvas.nativeElement;
+    this.canvasContext = this.canvasElement.getContext('2d');
+    this.videoElement = this.video.nativeElement;
+  }
 
   async takePicture() {
     const image = await Camera.getPhoto({
@@ -24,6 +54,10 @@ export class HomePage {
     });
 
     this.myImage = image.webPath;
+  }
+
+  async deleteImage(){
+    this.myImage = null ;
   }
 
   async getCurrentPosition() {
@@ -39,6 +73,118 @@ export class HomePage {
         ${this.position.coords.latitude}, 
         ${this.position.coords.longitude}`      
     });
+  }
+
+  async showQrToast() {
+    const toast = await this.toastCtrl.create({
+      message: `Open ${this.scanResult}?`,
+      position: 'top',
+      duration: 1000,
+      buttons: [
+        {
+          text: 'Open',
+          handler: () => {
+            window.open(this.scanResult, '_system', 'location=yes');
+          }
+        }
+      ]
+    });
+    toast.present();
+  }
+ 
+  reset() {
+    this.scanResult = null;
+  }
+ 
+  stopScan() {
+    this.scanActive = false;
+  }
+
+  async startScan() {
+    // Not working on iOS standalone mode!
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: 'environment' }
+    });
+   
+    this.videoElement.srcObject = stream;
+    // Required for Safari
+    this.videoElement.setAttribute('playsinline', true);
+   
+    this.loading = await this.loadingCtrl.create({});
+    await this.loading.present();
+   
+    this.videoElement.play();
+    requestAnimationFrame(this.scan.bind(this));
+  }
+   
+  async scan() {
+    if (this.videoElement.readyState === this.videoElement.HAVE_ENOUGH_DATA) {
+      if (this.loading) {
+        await this.loading.dismiss();
+        this.loading = null;
+        this.scanActive = true;
+      }
+   
+      this.canvasElement.height = this.videoElement.videoHeight;
+      this.canvasElement.width = this.videoElement.videoWidth;
+   
+      this.canvasContext.drawImage(
+        this.videoElement,
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert'
+      });
+   
+      if (code) {
+        this.scanActive = false;
+        this.scanResult = code.data;
+        this.showQrToast();
+      } else {
+        if (this.scanActive) {
+          requestAnimationFrame(this.scan.bind(this));
+        }
+      }
+    } else {
+      requestAnimationFrame(this.scan.bind(this));
+    }
+  }
+
+  captureImage() {
+    this.fileinput.nativeElement.click();
+  }
+   
+  handleFile(files: FileList) {
+    const file = files.item(0);
+   
+    var img = new Image();
+    img.onload = () => {
+      this.canvasContext.drawImage(img, 0, 0, this.canvasElement.width, this.canvasElement.height);
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert'
+      });
+   
+      if (code) {
+        this.scanResult = code.data;
+        this.showQrToast();
+      }
+    };
+    img.src = URL.createObjectURL(file);
   }
 
 }
